02d3701510fd7d04e1b5ca3db485d4780ae2fd6a
Kamilovkz <001kamilov@gmail.com>
2025-02-19 18:25:03 +0500

    update schemas and add photo

--- schemas.py
+++ schemas.py
@@ -23,6 +23,105 @@ d = {
     },
 }
 
+d2 = {
+    "update_id": 894139849,
+    "message": {
+        "message_id": 43,
+        "from": {
+            "id": 398685944,
+            "is_bot": False,
+            "first_name": "Melon",
+            "username": "qweerty01",
+            "language_code": "en",
+        },
+        "chat": {
+            "id": 398685944,
+            "first_name": "Melon",
+            "username": "qweerty01",
+            "type": "private",
+        },
+        "date": 1739883162,
+        "sticker": {
+            "width": 512,
+            "height": 389,
+            "emoji": "😏",
+            "set_name": "Shitsshitsshitsshists",
+            "is_animated": False,
+            "is_video": False,
+            "type": "regular",
+            "thumbnail": {
+                "file_id": "AAMCBAADGQEAAytntIKaKNhjBo3hjYT4m0lXiogi0AAC_QADa65eCRchwD8IWTtOAQAHbQADNgQ",
+                "file_unique_id": "AQAD_QADa65eCXI",
+                "file_size": 3618,
+                "width": 128,
+                "height": 97,
+            },
+            "thumb": {
+                "file_id": "AAMCBAADGQEAAytntIKaKNhjBo3hjYT4m0lXiogi0AAC_QADa65eCRchwD8IWTtOAQAHbQADNgQ",
+                "file_unique_id": "AQAD_QADa65eCXI",
+                "file_size": 3618,
+                "width": 128,
+                "height": 97,
+            },
+            "file_id": "CAACAgQAAxkBAAMrZ7SCmijYYwaN4Y2E-JtJV4qIItAAAv0AA2uuXgkXIcA_CFk7TjYE",
+            "file_unique_id": "AgAD_QADa65eCQ",
+            "file_size": 24182,
+        },
+    },
+}
+
+d3 = {
+    "update_id": 894139851,
+    "message": {
+        "message_id": 45,
+        "from": {
+            "id": 398685944,
+            "is_bot": False,
+            "first_name": "Melon",
+            "username": "qweerty01",
+            "language_code": "en",
+        },
+        "chat": {
+            "id": 398685944,
+            "first_name": "Melon",
+            "username": "qweerty01",
+            "type": "private",
+        },
+        "date": 1739883696,
+        "photo": [
+            {
+                "file_id": "AgACAgIAAxkBAAMtZ7SEsO8P8zLJQT0frQLeP0yuH18AAt3mMRshkqhJe0FU3_i92KMBAAMCAANzAAM2BA",
+                "file_unique_id": "AQAD3eYxGyGSqEl4",
+                "file_size": 1441,
+                "width": 90,
+                "height": 70,
+            },
+            {
+                "file_id": "AgACAgIAAxkBAAMtZ7SEsO8P8zLJQT0frQLeP0yuH18AAt3mMRshkqhJe0FU3_i92KMBAAMCAANtAAM2BA",
+                "file_unique_id": "AQAD3eYxGyGSqEly",
+                "file_size": 23892,
+                "width": 320,
+                "height": 248,
+            },
+            {
+                "file_id": "AgACAgIAAxkBAAMtZ7SEsO8P8zLJQT0frQLeP0yuH18AAt3mMRshkqhJe0FU3_i92KMBAAMCAAN4AAM2BA",
+                "file_unique_id": "AQAD3eYxGyGSqEl9",
+                "file_size": 114939,
+                "width": 800,
+                "height": 619,
+            },
+            {
+                "file_id": "AgACAgIAAxkBAAMtZ7SEsO8P8zLJQT0frQLeP0yuH18AAt3mMRshkqhJe0FU3_i92KMBAAMCAAN5AAM2BA",
+                "file_unique_id": "AQAD3eYxGyGSqEl-",
+                "file_size": 200910,
+                "width": 1170,
+                "height": 906,
+            },
+        ],
+        "caption": "SUPERASTANA",
+    },
+}
+
 
 class From(BaseModel):
     from_id: int = Field(alias="id")
@@ -40,12 +139,23 @@ class Chat(BaseModel):
     type: str
 
 
+class Photo(BaseModel):
+    file_id: str
+    file_unique_id: str
+    file_size: int
+    width: int
+    height: int
+
+
 class Message(BaseModel):
     message_id: int
     fromTg: From = Field(alias="from")
     chat: Chat
     date: int
-    text: str
+    text: str = None
+    sticker: dict = None
+    photo: list[Photo] = None
+    caption: str = None
 
 
 class Answer(BaseModel):
@@ -53,5 +163,5 @@ class Answer(BaseModel):
     message: Message
 
 
-# obj = Answer.model_validate(d)
-# print(obj.message.chat.chat_id)
+# obj = Answer.model_validate(d3)
+# print(obj.message.photo[-1])
